3e16927093b03c29e4c812bedeeb4c177bd99e38
Peixuan Liu <peixuan@usc.edu>
2017-06-06 16:27:58 +0800

    completed cpu_percent_static.py

--- Monitor_my_laptop_using_ML/README.md
+++ Monitor_my_laptop_using_ML/README.md
@@ -1,2 +1,6 @@
 # Personal project
-monitor computer status using machine learning
+This folder includes programs to monitor computer status using machine learning
+
+# cpu_percent_static.py
+This program will monitor the CPU %utilization every 2 seconds for a total of 2 minutes and save the data in cpu_percent_data.txt in current directory. Then plot a graph which reflects the data.  
+
---
+++ Monitor_my_laptop_using_ML/cpu_percent_static.py
@@ -0,0 +1,54 @@
+import psutil
+import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#get CPU %utilization from the system and save the data in a file
+#get data every interval seconds, stop when tol_time is using up
+def fetchCPUPercent(fileName, interval, tol_time):
+	t = round(tol_time/interval);
+	var = 0
+	dataFile = open(fileName,'w')
+	dataFile.write('')
+	dataFile.close()
+	dataFile = open(fileName,'a')
+
+	stime = round(time.time())
+	while var < t:
+		dataFile.write(str(round(time.time())-stime))
+		dataFile.write(',')
+		dataFile.write(str(psutil.cpu_percent(interval=None)))
+		dataFile.write('\n')
+		time.sleep(interval)
+		var = var+1;
+		
+	dataFile.close()
+
+#read the saved data from cpu_percent_data.txt to 2 arrays
+def loadData(fileName):
+	inFile = open(fileName, 'r')
+	
+	#defile 2 empty arrays to store the data
+	X = []
+	y = []
+	
+	for line in inFile:
+		trainingSet = line.split(',')
+		X.append(trainingSet[0])
+		y.append(trainingSet[1])
+		
+	return (X, y)
+	
+def plotData(X, y):
+	#fig, ax = plt.subplots()
+
+	#ax.fill(X, y, zorder=10)
+	#ax.grid(True, zorder=5)
+	plt.plot(X, y)
+	plt.show()
+
+# fetch cpu utilization percentage data every 2 seconds in 2 minutes
+fetchCPUPercent('cpu_percent_data.txt', 2, 120)
+(X, y) = loadData('cpu_percent_data.txt')
+plotData(X, y)
