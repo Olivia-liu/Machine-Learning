e5fce82560caa470d39bc231e15e5828d5946ed6
Peixuan Liu <peixuan@usc.edu>
2017-08-17 21:45:25 -0700

    test one of the most popular solutions online, but doesn't have much improvement

--- Tragedy_v2/main.py
+++ Tragedy_v2/main.py
@@ -1,147 +1,212 @@
-#-------------- machine learning program STARTS here --------------#
-from utility import *
-import tensorflow as tf
-from numpy.random import RandomState
+# Credit: https://www.kaggle.com/startupsci/titanic-data-science-solutions
 
-#-------------- Define some global variables --------------#
-age_idx = 2
-sex_idx = 1
-fare_idx = 5
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
 
-#-------------- read data from csv files --------------#
-X_all = get_data('train_X.csv')
-y_all = get_data('train_y.csv')
-X_submit = get_data('submit_X.csv')
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-#-------------- fill empty features, -1 for female, 1 for male --------------#
-total_age = 0
-num_people = 0
-#calculate average age
-for row in X_all:
-	if(row[age_idx] != ''):
-		total_age = total_age + float(row[age_idx])
-		num_people = num_people + 1
-average_age = total_age/num_people
-#fill empty ages with the average age
-#set female to be -1, male to be 1
-for row in X_all:
-	if(row[age_idx] == ''):
-		row[age_idx] = str(average_age)
-	if(row[sex_idx] == 'female'):
-		row[sex_idx] = '-1'
-	else:
-		row[sex_idx] = '1'
-		
-for row in X_submit:
-	if(row[age_idx] == ''):
-		row[age_idx] = str(average_age)
-	if(row[sex_idx] == 'female'):
-		row[sex_idx] = '-1'
-	else:
-		row[sex_idx] = '1'
-		
-#convert lists to arrays
-X_all = np.array(X_all, dtype = float)
-y_all = np.array(y_all, dtype = float)
-X_submit = np.array(X_submit, dtype = float)
-		
-#-------------- split train, cross-validation and test sets --------------#
-X_train = X_all[0:891]
-y_train = y_all[0:891]
-dataset_size = 891
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+combine = [train_df, test_df]
 
-X_val = X_all[534:712]
-y_val = y_all[534:712]
+train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
+test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
+combine = [train_df, test_df]
 
-X_test = X_all[712:891]
-y_test = y_all[712:891]
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
 
-#-------------- normalize the training set --------------#
-normalize(X_train, age_idx)
-normalize(X_train, fare_idx)
-normalize(X_submit, age_idx)
-normalize(X_submit, fare_idx)
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
 
-import csv
-outputFile = open('output.csv', 'w', newline='')
-outputWriter = csv.writer(outputFile)
-for row in X_submit:
-	outputWriter.writerow(row)
-outputFile.close()
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+# We can convert the categorical titles to ordinal.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
 
-#print(y_train)
-#-------------- Tensorflow to calculate weights STARTS --------------#
-batch_size = 8
-
-# define neural network parameters (weights)
-w1 = tf.Variable(tf.random_normal([6,12], stddev=1))
-w2 = tf.Variable(tf.random_normal([12,12], stddev=1))
-w3 = tf.Variable(tf.random_normal([12,1], stddev=1))
-biases1 = tf.Variable(tf.constant(0.1, shape=[12]))
-biases2 = tf.Variable(tf.constant(0.1, shape=[12]))
-biases3 = tf.Variable(tf.constant(0.1, shape=[1]))
-
-# Use none for small batch
-x = tf.placeholder(tf.float32, shape=(None, 6), name='x-input')
-y_ = tf.placeholder(tf.float32, shape=(None, 1), name='y-input')
-
-# define forward propagation(sigmoid)
-a1 = tf.sigmoid(tf.matmul(x, w1)+biases1)
-a2 = tf.sigmoid(tf.matmul(a1, w2)+biases2)
-y = tf.sigmoid(tf.matmul(a2, w3)+biases3)
-
-# define cost function and back propagation algorithm
-cross_entropy = tf.reduce_mean(
-	-y_*tf.log(tf.clip_by_value(y, 1e-10, 1.0)) - (1-y_)*tf.log(tf.clip_by_value(1-y, 1e-10, 1.0)))
-#cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_)
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
-
-# create a session
-with tf.Session() as sess:
-	init_op = tf.initialize_all_variables()
-	sess.run(init_op)
-
-	STEPS = 30000
-	for i in range(STEPS):
-		start = (i * batch_size) % dataset_size
-		end = min(start+batch_size, dataset_size)
-		
-		sess.run(train_step,
-				feed_dict={x: X_train[start:end], y_: y_train[start:end]})
-		if i%1000 == 0:
-			total_cross_entropy = sess.run(
-				cross_entropy, feed_dict={x: X_train, y_: y_train})
-			print("After %d training steps, cross entropy on all data is %g" %
-				(i, total_cross_entropy))
-			
-	#print(sess.run(
-	#			cross_entropy, feed_dict={x: X_val, y_: y_val}))		
-	#print(sess.run(
-	#			cross_entropy, feed_dict={x: X_test, y_: y_test}))					
-	print(sess.run(w1))
-	print(sess.run(w2))
-	print(sess.run(w3))
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
 	
-	print(biases1.eval())
-	print(biases2.eval())
-	print(biases3.eval())
+train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+combine = [train_df, test_df]
+
+# Converting a categorical feature
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+# Complete age
+guess_ages = np.zeros((2,3))
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & \
+                                  (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            # age_mean = guess_df.mean()
+            # age_std = guess_df.std()
+            # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+
+            age_guess = guess_df.median()
+
+            # Convert random age float to nearest .5 age
+            guess_ages[i,j] = int( age_guess/0.5 + 0.5 ) * 0.5
+            
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[ (dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1),\
+                    'Age'] = guess_ages[i,j]
+
+    dataset['Age'] = dataset['Age'].astype(int)
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
+for dataset in combine:    
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age']
+train_df = train_df.drop(['AgeBand'], axis=1)
+combine = [train_df, test_df]
+
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+for dataset in combine:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
 	
-	#weight1 = sess.run(w1)
-	#weight2 = sess.run(w2)
+train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+combine = [train_df, test_df]
+
+for dataset in combine:
+    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+
+freq_port = train_df.Embarked.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+
+train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+
+for dataset in combine:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+train_df = train_df.drop(['FareBand'], axis=1)
+combine = [train_df, test_df]
+    
+X_train = train_df.drop("Survived", axis=1)
+Y_train = train_df["Survived"]
+X_test  = test_df.drop("PassengerId", axis=1).copy()
+X_train.shape, Y_train.shape, X_test.shape
+
+# Logistic Regression
+
+logreg = LogisticRegression()
+logreg.fit(X_train, Y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+
+coeff_df = pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns = ['Feature']
+coeff_df["Correlation"] = pd.Series(logreg.coef_[0])
+
+# Support Vector Machines
+
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+
+# Gaussian Naive Bayes
+
+gaussian = GaussianNB()
+gaussian.fit(X_train, Y_train)
+Y_pred = gaussian.predict(X_test)
+acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+
+# Perceptron
+
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+
+# Linear SVC
+
+linear_svc = LinearSVC()
+linear_svc.fit(X_train, Y_train)
+Y_pred = linear_svc.predict(X_test)
+acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
+
+# Stochastic Gradient Descent
+
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+
+# Decision Tree
+
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+
+# Random Forest
+
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+random_forest.score(X_train, Y_train)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 
+              'Random Forest', 'Naive Bayes', 'Perceptron', 
+              'Stochastic Gradient Decent', 'Linear SVC', 
+              'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log, 
+              acc_random_forest, acc_gaussian, acc_perceptron, 
+              acc_sgd, acc_linear_svc, acc_decision_tree]})
+
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred
+    })
+
 	
-	#prediction = y
-	#print ("predictions", prediction.eval(feed_dict={x: X_train, y_: y_train}, session=sess))
-	#print(y_train)
-	
-
-#-------------- Tensorflow to calculate weights ENDS --------------#
-
-#-------------- calculate predictions --------------#
-
-
-
-#-------------- machine learning program ENDS here --------------#
-	
-
-
+submission.to_csv('submission.csv', index=False)
